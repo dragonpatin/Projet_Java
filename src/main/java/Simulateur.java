a28660b39f004cda325bf21e8c66e005348c8b6b
Jean-Charles SOTTAS <jc.sottas@gmail.com>
2018-04-24 17:43:58 +0200

    Modification pour que cela compile

--- src/main/java/Simulateur.java
+++ src/main/java/Simulateur.java
@@ -7,14 +7,7 @@ import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.*;
 public class Simulateur{
-	int prixHeureCreuse
-	 int prixHeurePic
-	 int consommationTotale
-	 entresortie S
-	 SimpleDateFormat date
-	 int consommationANePasDepasser
-	 vector ConsommationObjet
-	 vector Objet
+
 
 
 }
