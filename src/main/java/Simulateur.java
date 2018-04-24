27afc6159d4c9b8b1ddf9842842ac19b200ac788
Jean-Charles SOTTAS <jc.sottas@gmail.com>
2018-04-24 17:30:43 +0200

    Début du projet. Création de toute les classes

---
+++ src/main/java/Simulateur.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.*;
+public class Simulateur{
+	int prixHeureCreuse
+	 int prixHeurePic
+	 int consommationTotale
+	 entresortie S
+	 SimpleDateFormat date
+	 int consommationANePasDepasser
+	 vector ConsommationObjet
+	 vector Objet
+
+
+}
