4a8345b5dc2e57667899d252bb41249ecfa10c4c
Jean-Charles SOTTAS <jc.sottas@gmail.com>
2018-04-24 20:55:21 +0200

    Ajout de toute les fonctions dans Simulateur

--- src/main/java/Simulateur.java
+++ src/main/java/Simulateur.java
@@ -6,7 +6,79 @@ import java.io.IOException;
 import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.*;
+import java.util.*;
+import java.text.SimpleDateFormat;
 public class Simulateur{
+    int prixHeureCreuse;
+   int prixHeurePic;
+    int consommationTotale;
+    entresortie S;
+    SimpleDateFormat date;
+    int consommationANePasDepasser;
+    Vector<String> ConsommationObjet;
+    Vector<Objet> objet;
+    int temperatureExterieur;
+
+    public Simulateur( entresortie e){
+
+    }
+    public void recherchePrix (){
+
+    }
+    public void calculConsommation (){
+
+    }
+    public void recupereObjet (){
+
+    }
+    public void miseAJourObjet (){
+
+    }
+    public void ExtinctionAutomatique (int ConsoMax){
+
+    }
+    public void recupereTemperatureExt (){
+
+    }
+    public Vector getConsommation (){
+        return null;
+    }
+    public int getPrixHC (){
+        return 0;
+    }
+    public int getPrixHP (){
+        return 0;
+    }
+    public int ConsommationANePasDepasser ( ){
+        return 0;
+    }
+    public int consommationTotale (){
+        return 0;
+    }
+    public void rechercheDateHeure (){
+
+    }
+    public SimpleDateFormat getDate (){
+        return null;
+    }
+    public Vector ConsommationObjet (){
+        return null;
+    }
+    public int getTemperatureExt (){
+        return 0;
+    }
+    public Vector getObjet (){
+        return null;
+    }
+    public Vector consoJour (){
+        return null;
+    }
+    public Vector ConsoMois (){
+        return null;
+    }
+    public Vector ConsoSemaine (){
+        return null;
+    }
 
 
 
