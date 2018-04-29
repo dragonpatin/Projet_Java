1b72f414c7cfcb49f084a47c94dd6b1a1bfbd307
Thibaud larcher <thibaud.larcher@ens.uvsq.fr>
2018-04-29 16:57:34 +0200

    Modification du fichier entrée sortie

--- src/main/java/entresortie.java
+++ src/main/java/entresortie.java
@@ -1,25 +1,83 @@
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
 import java.util.*;
+import javax.swing.JFileChooser;
+
 
 public class entresortie{
 
-        String NomFichier;
-        Vector ListeObjet;
-        Vector ObjetFavoris;
-        Vector Preference;
-        Vector consoJour;
-        Vector ConsoMois;
-        Vector ConsoSemaine;
+    File NomFichier;
+    Vector ListeObjet;
+    Vector ObjetFavoris;
+    Vector Preference;
+    Vector consoJour;
+    Vector ConsoMois;
+    Vector ConsoSemaine;
 
-    entresortie (){
-
+    public entresortie(String Nom)
+    {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(new File("Sauvegarde.txt")));
+            writer.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        this.NomFichier = new File("Sauvegarde.txt");
     }
-    entresortie ( String NomFichier){
 
+    public entresortie(){
+        JFileChooser dialogue = new JFileChooser();
+        dialogue.showOpenDialog(null);
+        System.out.println("Fichier choisi : " + dialogue.getSelectedFile());
+        this.NomFichier = dialogue.getSelectedFile();
     }
-    public void lecturefichier(String NomFichier){
 
+    public void lecturefichier(File NomFichier, String donnees){
+        File file = NomFichier;
+        FileReader fr;
+        String str;
+        if(donnees == "Objet"){
+
+        }
+        else if(donnees == "ObjetFavori"){
+
+        }
+        else if(donnees == "Preference"){
+
+        }
+        else if(donnees == "consoJour"){
+
+        }
+        else if(donnees == "ConsoMois"){
+
+        }
+        else if(donnees == "ConsoSemaine"){
+
+        }
+        try{
+            //Création de l'objet de lecture
+            fr = new FileReader(file);
+            str = "";
+            int i = 0;
+            //Lecture des données
+            while((i = fr.read()) != -1)
+                str += (char)i;
+
+            //Affichage
+            System.out.println(str);
+
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    public void ecriture ( String NomFichier){
+    public void ecriture ( File NomFichier){
 
     }
     public void modifieObjet ( Vector ListeObjet ){
