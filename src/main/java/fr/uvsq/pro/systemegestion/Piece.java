fde2ff1cebd8d29526d08d21ef0404015316ae83
Nicolas Carbonnier <nicolas@macbook-air-de-nicolas.home>
2018-05-20 15:09:05 +0200

    Ajout de la classe Piece

--- src/main/java/fr/uvsq/pro/systemegestion/Piece.java
+++ src/main/java/fr/uvsq/pro/systemegestion/Piece.java
@@ -10,39 +10,58 @@ public class Piece{
     int consoPiece;
 
 
-    Piece(String n, int e, Simulateur S ){
-
+    Piece(String n, int e, Simulateur S){
+        this.nom = n;
+        this.etage = e;
+        this.S = S;
+        this.ObjetPiece = new Vector<Objet>();
+        this.NombreObjet = 0;
+        this.consoPiece = 0;
     }
 
-
-    public void changerNom( String n){
-
+    public void changerNom(String n){
+        this.nom = n;
     }
+
     public void changerEtage(int e){
-
+        this.etage = e;
     }
-    public void calculConsoPiece (){
 
+    public void calculConsoPiece(){
+        int i = 0;
+        for(i=0; i<ObjetPiece.size(); i++){
+            consoPiece += ObjetPiece.get(i).getConsommation();
+        }
     }
+
     public void ajoutObjet(Objet O){
-
+        ObjetPiece.add(O);
+        this.NombreObjet++;
+        calculConsoPiece();
     }
+
     public void SupprimerObjet(Objet O){
+        ObjetPiece.remove(O);
+        this.NombreObjet--;
+        calculConsoPiece();
+    }
 
+    public int  getEtage(){
+        return this.etage;
     }
-    public int  getEtage (){
-        return 0;
+
+    public int  NombreObjet(){
+        return this.NombreObjet;
     }
-    public int  NombreObjet (){
-        return 0;
-    }
-    public Vector getObjetPiece (){
+
+    public Vector getObjetPiece(){
         return ObjetPiece;
     }
-    public int getConsoPiece (){
-        return 0;
+    public int getConsoPiece(){
+        return this.consoPiece;
     }
+
     public String getNom(){
-        return null;
+        return this.nom;
     }
 }
